d039efd42962c98d807740b0bb4959d26fef1782
xy <pj.soft.dev.x@gmail.com>
2026-07-24 20:35:30 +0200

    permissions, more debug

--- scripts/flatpak-prepare.sh
+++ scripts/flatpak-prepare.sh
@@ -21,7 +21,13 @@ echo WD:
 pwd
 
 cat io.github.pjdude.SimpleAudioSweeper.template.yml | replace COMMIT_SHA_TO_REPLACE $COMMIT_SHA | replace VERSION_TO_REPLACE $VERSION > io.github.pjdude.SimpleAudioSweeper.yml
+chmod -v 777 io.github.pjdude.SimpleAudioSweeper.yml
 echo io.github.pjdude.SimpleAudioSweeper.yml
 
 cat io.github.pjdude.SimpleAudioSweeper.metainfo.template.xml | replace VERSION_TO_REPLACE $VERSION | replace DATE_TO_REPLACE $DATE > io.github.pjdude.SimpleAudioSweeper.metainfo.xml
+chmod -v 777 io.github.pjdude.SimpleAudioSweeper.metainfo.xml
 echo io.github.pjdude.SimpleAudioSweeper.metainfo.xml
+
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ls -l
+echo +++++++++++++++++++++++++++++++++++++++++++++++++++++++
